d9617847d6926f19c5ea570d619ba74bf84436e8
Samuel Taiwo <samueltaiwo@stdev.co.uk>
2025-06-28 05:55:40 +0100

    [Cleanup] created initial implementation for desktop and wasm build targets

--- composeApp/src/desktopMain/kotlin/ship/f/project/x/client/main.kt
+++ composeApp/src/desktopMain/kotlin/ship/f/project/x/client/main.kt
@@ -1,13 +1,21 @@
 package ship.f.project.x.client
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import ship.f.engine.client.utils.serverdrivenui.RenderingContext.CommonClient
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "ProjectX",
     ) {
-//       App()
+        val config = remember(Unit) { Initialization().config }
+        val client = remember(Unit) { CommonClient().apply { init(config) } }
+
+        App(
+            client = client,
+            config = config,
+        )
     }
 }
--- composeApp/src/wasmJsMain/kotlin/ship/f/project/x/client/main.kt
+++ composeApp/src/wasmJsMain/kotlin/ship/f/project/x/client/main.kt
@@ -1,12 +1,20 @@
 package ship.f.project.x.client
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import ship.f.engine.client.utils.serverdrivenui.RenderingContext.CommonClient
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-//        App()
+        val config = remember(Unit) { Initialization().config }
+        val client = remember(Unit) { CommonClient().apply { init(config) } }
+
+        App(
+            client = client,
+            config = config,
+        )
     }
 }
