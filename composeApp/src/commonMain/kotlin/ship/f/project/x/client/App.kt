6b90c6a68d8d70023afa5c85fa1a408173cdb7c1
Samuel Taiwo <samueltaiwo@stdev.co.uk>
2025-06-29 11:15:59 +0100

    [Cleanup] removed nullability of current screen in favour of a blank screen as a sensible default

--- composeApp/src/commonMain/kotlin/ship/f/project/x/client/App.kt
+++ composeApp/src/commonMain/kotlin/ship/f/project/x/client/App.kt
@@ -7,8 +7,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import ship.f.engine.client.utils.serverdrivenui.RenderScreen
 import ship.f.engine.client.utils.serverdrivenui.CommonClient
+import ship.f.engine.client.utils.serverdrivenui.RenderScreen
 import ship.f.engine.client.utils.serverdrivenui.components.ServerDrivenUITheme
 
 @Composable
@@ -17,9 +17,8 @@ fun App(
 ) {
     Column {
         Spacer(modifier = Modifier.height(32.dp))
-        val config = client.currentScreen!!
         RenderScreen(
-            screenConfig = config,
+            screenConfig = client.currentScreen, // MutableState causes recomposition on every change to it
             client = client
         )
     }
